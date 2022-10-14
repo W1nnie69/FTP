ab522d80ad6955f247149c70c8b56d0fbe03122d
W1nnie69 <109217805+W1nnie69@users.noreply.github.com>
2022-10-14 19:49:18 +0800

    Initial commit

---
+++ client.py
@@ -0,0 +1,149 @@
+from inspect import getfile
+from itertools import tee
+import socket
+import os
+import tqdm
+import tkinter as tk
+from tkinter import filedialog
+import json
+
+
+#ipaddressbook = {}
+#ipaddressbook = {'address': {'name':'defualt', 'ip':'0.0.0.0', 'id':1}}
+
+
+
+separator = "<SEPARATOR>"
+
+buffer_size = 4096
+
+
+
+def write_json(new_data, filename='mydata.json'):
+    with open(filename,'r+') as file:
+          # First we load existing data into a dict.
+        file_data = json.load(file)
+        # Join new_data with file_data inside emp_details
+        file_data["address"].append(new_data)
+        # Sets file's current position at offset.
+        file.seek(0)
+        # convert back to json.
+        json.dump(file_data, file, indent = 4)
+
+
+
+
+
+
+
+def getfilepath():
+    clientroot = tk.Tk()
+    clientroot.withdraw()
+    global filename
+    global filesize
+    filename = tk.filedialog.askopenfilename()
+    clientroot.destroy()
+    filesize = os.path.getsize(filename)
+    print(filename)
+    
+
+
+
+def connecttoserver():
+    global s
+
+    ipaddressbook = {"address":[{"name":"test", "ip":"0.0.0.0","id":"1"}]} 
+    
+    
+
+
+    qn = input("Use a saved ip address? (Y/N)"  )
+
+    if qn == "Y":
+        with open("mydata.json", "r") as f:
+            ipaddressbook = json.load(f)
+
+        
+        number = len(ipaddressbook["address"])
+        
+
+        for x in range(number):
+        
+            name = ipaddressbook["address"][x]["name"]
+            ipname = ipaddressbook["address"][x]["ip"]
+            idname = ipaddressbook["address"][x]["id"]
+            
+            print(idname,name,ipname,)
+            
+
+        
+
+        ans = int(input(""))
+        ip = ipaddressbook["address"][ans - 1]["ip"]
+
+        
+        
+        print("Ip address", ip, "selected")
+    
+
+
+    elif qn == "N":
+        print("cock")
+
+        
+
+        ip = input("Please enter the receiver's ip address= ")
+        name = input("Please enter the receiver's name= ")
+    
+        newlist ={
+                    
+                    "name":name,
+                    "ip":ip,
+                    "id":str(len(ipaddressbook)+1)
+                    
+                 }
+                
+        
+        
+        write_json(newlist)
+
+                
+
+        
+        
+
+    else:
+        print(".")
+        
+    
+    
+    port = input("Please enter the port you are using(Default port is 6969): ") or 6969
+    p = int(port)
+    print(f"[+] Connecting to {ip}:{port}")
+    s =  socket.socket()
+    s.connect((ip, p))
+    s.send(f"{filename}{separator}{filesize}".encode())
+    
+
+
+
+def send():
+    progress = tqdm.tqdm(range(filesize), f"Sending {filename}", unit="B", unit_scale=True, unit_divisor=1024)
+
+    with open(filename, "rb") as f:
+        while True:
+        
+            bytes_read = f.read(buffer_size)
+            if not bytes_read:
+                break
+        
+            s.sendall(bytes_read)
+       
+            progress.update(len(bytes_read))
+            
+    s.close()
+    
+
+
+connecttoserver()
+
---
+++ server.py
@@ -0,0 +1,59 @@
+import re
+import socket
+import tqdm
+import os
+import tkinter as tk
+
+
+buffer_size = 4096
+
+separator = "<SEPARATOR>"
+
+
+def receivergui():
+    receiverroot = tk.Tk()
+    receiverroot.title("The better Google drive")
+
+    canvas1 = tk.Canvas(receiverroot, width=800, height=700)
+    canvas1.pack()
+
+    img1 = tk.PhotoImage(file="sussy.gif")
+    canvas1.create_image(233, 344, image=img1)
+
+    receiverroot.mainloop()
+
+
+def startserver():
+    myip = input("Please enter your own ip address(Leave it empty if you are not sure): ") or "0.0.0.0"
+    myport = int(input("Please enter the port you are using(Default port is 6969): ") or 6969)
+    print(myip)
+    print(myport)
+    s = socket.socket()
+    s.bind((myip, myport))
+    s.listen(5)
+    print(f"[*] Listening as {myip}:{myport}")
+    client_socket, address = s.accept()
+    print(f"[+] {address} is connected.")
+    received = client_socket.recv(buffer_size).decode()
+    filename, filesize = received.split(separator)
+    filename = os.path.basename(filename)
+    filesize = int(filesize)
+
+
+    progress = tqdm.tqdm(range(filesize), f"Receiving {filename}", unit="B", unit_scale=True, unit_divisor=1024)
+    with open(filename, "wb") as f:
+        while True:
+            bytes_read = client_socket.recv(buffer_size)
+
+            if not bytes_read:
+                break
+
+            f.write(bytes_read)
+            progress.update(len(bytes_read))
+
+    client_socket.close()
+    s.close()
+
+
+
+
