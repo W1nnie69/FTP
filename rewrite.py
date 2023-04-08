eba902e1a88f207cd557a253f8c9fca7922a7522
W1nnie69 <danizye@gmail.com>
2023-04-08 11:25:50 +0800

    ver 1.06
    added alot of shit

--- rewrite.py
+++ rewrite.py
@@ -2,6 +2,15 @@ import time, socket, os ,tqdm, json
 import customtkinter as ctk
 from PIL import Image
 import tkinter as tk
+import threading
+
+
+
+
+
+
+
+
 
 
 class FTPApp(ctk.CTk):
@@ -9,13 +18,13 @@ class FTPApp(ctk.CTk):
         super().__init__()
         
         global size
-        size = "900x700"
+        size = "900x500"
 
         self.geometry(size)
         self.title("FTP")
 
-        self.grid_rowconfigure(0, weight=1)  # configure grid system
-        self.grid_columnconfigure(0, weight=1)
+        # self.grid_rowconfigure(0, weight=1)  # configure grid system
+        # self.grid_columnconfigure(0, weight=1)
         
         self._frame = None
         self.switch_frames(mainframe)
@@ -50,24 +59,107 @@ class FTPApp(ctk.CTk):
 
 
 
+
+
+
+
+
 class mainframe(ctk.CTkFrame):
     def __init__(self, master, *args, **kwargs):
         super().__init__(master, *args, **kwargs)
 
         self.configure(fg_color="#242424")
         
-        self.grid_rowconfigure(0, weight=1)
-        self.grid_columnconfigure(0, weight=1)
+        # self.grid_rowconfigure(0, weight=1)
+        # self.grid_columnconfigure(0, weight=1)
 
         self.senderbutton = ctk.CTkButton(self, width=150, height=80, text="Send file", command=lambda: master.switch_frames(senderframe))
-        self.senderbutton.grid(row=0, column=0, padx=10, sticky="nsew")
+        self.senderbutton.grid(row=0, column=0, padx=(280,25), pady=(120,120))
+
         
+
         self.receiverbutton = ctk.CTkButton(self, width=150, height=80, text="Receive file")
-        self.receiverbutton.grid(row=0, column=1, padx=10, sticky="nsew")
+        self.receiverbutton.grid(row=0, column=1, padx=(1,1))
 
     def removeframe(self):
         self.grid_forget()
         FTPApp.gotosenderframe(self)
+
+
+
+
+
+
+
+
+
+
+
+
+    
+class ToplevelWindow(ctk.CTkToplevel):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        
+        self.geometry("400x350")
+
+
+
+
+
+        self.nameentry = ctk.CTkEntry(self, placeholder_text="Name goes here")
+        self.ipentry = ctk.CTkEntry(self, placeholder_text="Ip goes here")
+        self.savebutton = ctk.CTkButton(self, text="save", width=30, command=lambda: self.updatejson())
+
+        self.nameentry.grid(row=0, column=0)
+        self.ipentry.grid(row=1, column=0)
+        self.savebutton.grid(row=0, column=1)
+
+
+
+
+
+
+    def write_json(self, new_data, filename='mydata.json'):
+        with open(filename,'r+') as file:
+            # First we load existing data into a dict.
+            file_data = json.load(file)
+            # Join new_data with file_data inside emp_details
+            file_data["address"].append(new_data)
+            # Sets file's current position at offset.
+            file.seek(0)
+            # convert back to json.
+            json.dump(file_data, file, indent = 4)
+
+
+
+
+    def updatejson(self):
+        with open("mydata.json", "r") as f:
+                data = json.load(f)
+
+        name = self.nameentry.get()
+        ip = self.ipentry.get()
+        
+        newlist ={
+                    
+                        "name":name,
+                        "ip":ip,
+                        "id":str(len(data["address"])+1)
+                    
+                    }
+
+        print(newlist)
+
+        self.write_json(newlist)
+        self.destroy()
+
+
+
+
+
+
+
 
 
 
@@ -79,20 +171,39 @@ class senderframe(ctk.CTkFrame):
         self.configure(fg_color="#242424")
         
         
-        self.grid_rowconfigure(1, weight=0)
-        self.grid_columnconfigure(1, weight=0)
+        # self.grid_rowconfigure(1, weight=0)
+        # self.grid_columnconfigure(0, weight=0)
 
+        self.t = threading.Thread(target=self.sendfile)
+        self.t.daemon = True
         
         
-
         self.selectfilebutton = ctk.CTkButton(self, width=100, height=50, text="Select a file to send", command=lambda: self.getfilepath())
         
-        self.selectfilebutton.grid(row=1, column=1, sticky="nsew")
+        self.selectfilebutton.grid(row=0, column=0 ,pady=(100,0), padx=(15,0))
 
-        # self.filename = ctk.CTkLabel(self, text=file, pady=20)
-        # self.filename.grid(row=2,column=1,sticky="nsew")
+
+        self.label = ctk.CTkLabel(self, text="Select the receiver:")
+        self.label.grid(row=1, column=0, padx=(10,0), pady=(25,))
+
+        names = self.getname()
         
+        self.addroptions = ctk.CTkOptionMenu(self, values=names)
+        self.addroptions.grid(row=2, column=0, padx=(22,0), pady=(0,0))
 
+        
+        self.setbutton = ctk.CTkButton(self, text="set", width=40, height=30, command=lambda: self.setreceiverinfo())
+        self.setbutton.grid(row=2, column=1, padx=(10,0), pady=(0,0))
+
+        self.add_new_receiver = ctk.CTkButton(self, text="add", width=40, height=30, command=lambda: self.add())
+        self.add_new_receiver.grid(row=2, column=2, padx=(10,0), pady=(0,0))
+
+        self.sendbutton = ctk.CTkButton(self, text="Send the file :)", width=150, height=70, command=lambda: self.t.start())
+        self.sendbutton.grid(row=3, column=0, padx=(30,0), pady=(40,0))
+
+        self.toplevel_window = None
+
+    
 
     def getfilepath(self):
         clientroot = tk.Tk()
@@ -105,7 +216,141 @@ class senderframe(ctk.CTkFrame):
         print(filename)
         file = f"File {filename} is selected"
         self.filename = ctk.CTkLabel(self, text=file)
-        self.filename.grid(row=2,column=1,sticky="nsew")
+        self.filename.grid(row=0,column=1,pady=(90,0),padx=(10,0), columnspan=1000)
+
+
+
+    def getname(self):
+        with open('mydata.json', 'r') as f:
+            data = json.load(f)
+
+        addresses = data["address"]
+
+        address_names = [address["name"] for address in addresses]
+
+        return address_names
+
+
+
+
+
+
+
+    def on_option_changed(self):
+        print("Selected address:", self.addroptions.get())
+    
+
+
+
+
+
+
+    def setreceiverinfo(self):
+        global selected
+        global ip
+        selected = self.addroptions.get()
+        print(selected)  
+
+        with open('mydata.json', 'r') as f:
+            data = json.load(f)
+
+        ip = []
+
+        for address in data["address"]:
+            if address['name'] == selected:
+                getip = address['ip']
+                
+                ip.append(str(getip))
+                break
+        
+        
+        else:
+            print("error")
+
+        
+    
+
+
+
+
+    def start_sendfile_thread(self):
+        print('starting thread')
+        global stop_flag
+        stop_flag = threading.Event()
+        t = threading.Thread(target=self.sendfile())
+        t.start()
+        t.join()
+
+
+
+
+
+
+
+
+
+    def sendfile(self):
+        print(ip)
+        print("")
+
+        self.progressbar = ctk.CTkProgressBar(self, mode="determinate")
+        self.progressbar.grid(row=4, column=0, padx=(20,0), pady=(20,0))
+        self.progressbar.set(0)
+
+        realip = ip[0]
+
+        separator = "<SEPARATOR>"
+        buffer_size = 4096
+
+        port = 21
+
+        print(f"[+] Connecting to {realip}:{port}")
+        s = socket.socket()
+        s.connect((realip,port))
+        s.send(f"{filename}{separator}{filesize}".encode())
+
+        time.sleep(1)
+
+        progress = tqdm.tqdm(range(filesize), f"Sending {filename}", unit="B", unit_scale=True, unit_divisor=1024)
+
+        with open(filename, "rb") as f:
+            while True:
+            
+                bytes_read = f.read(buffer_size)
+                if not bytes_read:
+                    break
+            
+                s.sendall(bytes_read)
+        
+                progress.update(len(bytes_read))
+                
+        s.close()
+
+        stop_flag.set()
+
+
+
+
+
+
+
+
+
+    def add(self):
+        if self.toplevel_window is None or not self.toplevel_window.winfo_exists():
+            self.toplevel_window = ToplevelWindow(self)# create window if its None or destroyed
+            self.toplevel_window.focus()  
+        else:
+            self.toplevel_window.focus()  # if window exists focus it
+
+
+
+
+
+
+
+
+
 
 
 
@@ -118,7 +363,6 @@ class receiverframe(ctk.CTkFrame):
 
         self.grid_rowconfigure(0, weight=1)
         self.grid_columnconfigure(0, weight=1)
-
 
 
 
